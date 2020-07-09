6c96c6fde439e35572c3f2db13c47f117c0d15a7
Thalita Gonçalves <thagsousa15@gmail.com>
2020-07-09 12:58:57 -0300

    Separação dos links em um objeto

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+const mdLinks = (path) => {
+  fs.readFile(path, 'utf8', (err, file) => {
+    return new Promise((resolve, reject) => {
+      if (err) {
+        reject(err.message)
+      } else {
+        const regex = /(\[.[^[\]]*\])(\([^#][^)]+)/gm;
+        const match = file.match(regex);
+        if (match !== null) {
+          const arr = [];
+          match.map((str) => {
+            const arrLink = str.replace('[', '').split('](');
+            const strObject = { 
+            text:arrLink[0],
+            link:arrLink[1], 
+            file: path,
+           };
+            arr.push(strObject)
+          })
+          return resolve(arr);
+        }
+      }
+    })
+  })
+}
+
+mdLinks('/home/thalita/Documentos/SAP004-md-links/README.md')
+
+//module.exports = mdLinks();
+
