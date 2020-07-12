a771a4cbf56e2ec6318d94e3c7ff57bdc467e195
Thalita Gonçalves <thagsousa15@gmail.com>
2020-07-12 01:36:29 -0300

    Funcionalidade da CLI

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 const fs = require('fs');
+const path = require('path');
 
-const mdLinks = (path) => {
-  fs.readFile(path, 'utf8', (err, file) => {
-    return new Promise((resolve, reject) => {
-      if (err) {
-        reject(err.message)
-      } else {
-        const regex = /(\[.[^[\]]*\])(\([^#][^)]+)/gm;
-        const match = file.match(regex);
-        if (match !== null) {
-          const arr = [];
-          match.map((str) => {
-            const arrLink = str.replace('[', '').split('](');
-            const strObject = { 
-            text:arrLink[0],
-            link:arrLink[1], 
-            file: path,
-           };
-            arr.push(strObject)
-          })
-          return resolve(arr);
-        }
-      }
-    })
+function findMDFiles(dir, previousFiles) {
+  const diretorios = fs.readdirSync(dir);
+  const removeNodeModules = diretorios.indexOf('node_modules');
+  if (removeNodeModules > -1) {
+    diretorios.splice(removeNodeModules, 1)
+  }
+  return diretorios.reduce((getFiles, file) => {
+    const getDir = path.join(dir, file);
+    if (fs.statSync(getDir).isDirectory()) {
+      findMDFiles(getDir, getFiles);
+    } else if ((path.extname(file) === ".md")) {
+      getFiles.push(getDir);
+    }
+    return getFiles;
+  }, previousFiles || [])
+}
+
+const mdLinks = (diretorios) => {
+  return new Promise((resolve, reject) => {
+    const regex = /(\[.[^[\]]*\])(\([^#][^)]+)/gm;
+    const findedFiles = findMDFiles(diretorios, []);
+    if (findedFiles.length < 1) {
+      reject("We did not found any files.")
+    }
+    const results = findedFiles.map(fileMD => {
+      const file = fs.readFileSync(fileMD, 'utf8')
+      const match = file.match(regex);
+      return match.map((str) => {
+        const arrLink = str.replace('[', '').split('](');
+        const strObject = {
+          text: arrLink[0],
+          link: arrLink[1],
+          file: fileMD,
+        };
+        return strObject;
+      })
+    }
+    )
+    resolve(results[0])
   })
 }
 
-mdLinks('/home/thalita/Documentos/SAP004-md-links/README.md')
-
-//module.exports = mdLinks();
-
+module.exports = mdLinks;
---
+++ teste/teste.md
@@ -0,0 +1,3 @@
+[Google Keep](https://keep.google.com/)
+[Twitter](https://twitter.com/)
+[Letrux](https://music.youtube.com/playlist?list=OLAK5uy_kDIgAxQnIjnrAnTbHSwPAhuaP1Nwgy-DQ)
