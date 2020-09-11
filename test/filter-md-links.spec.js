f905e14ca91f16180d5fe3727596bc1bffac1449
Thalita Gonçalves <thagsousa15@gmail.com>
2020-09-11 14:08:03 -0300

    Correção de erros nos testes

--- test/filter-md-links.spec.js
+++ test/filter-md-links.spec.js
@@ -4,17 +4,17 @@ const mockLinks = [
   [{
     text: "Google Keep",
     link: "https://keep.google.com/",
-    file: "/home/thalita/Documentos/SAP004-md-links/teste/teste.md"
+    file: "teste/teste.md"
   },
   {
     text: "Twitter",
     link: "https://twitter.com/",
-    file: "/home/thalita/Documentos/SAP004-md-links/teste/teste.md"
+    file: "teste/teste.md"
   },
   {
     text: "Letrux",
     link: "https://music.youtube.com/playlist?list=OLAK5uy_kDIgAxQnIjnrAnTbHSwPAhuaP1Nwgy-DQ",
-    file: "/home/thalita/Documentos/SAP004-md-links/teste/teste.md"
+    file: "teste/teste.md"
   }]
 ]
 
@@ -25,7 +25,7 @@ describe('mdLinks', () => {
   });
 
   it('should return a list with text, link and path', () => {
-    return expect(mdLinks('/home/thalita/Documentos/SAP004-md-links/teste/')).resolves.toEqual(mockLinks);
+    return expect(mdLinks('./teste')).resolves.toEqual(mockLinks);
   })
 
   it('should show an error message when the return is an empty array', () => {
